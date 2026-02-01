0217c9decae1c2ce15d09660fd22fa35d1bcff71
Allan Vidal <alnvdl@users.noreply.github.com>
2026-02-01 13:08:32 +0000

    Try to remove locale configs

--- create.sh
+++ create.sh
@@ -38,22 +38,6 @@ mv $node_file node
 # Set shell config.
 cat <<EOF >> /home/alnvdl/.bashrc
 export PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] \\$ '
-
-export LANG=en_US.UTF-8
-export LANGUAGE=
-export LC_CTYPE=pt_BR.UTF-8
-export LC_NUMERIC=pt_BR.UTF-8
-export LC_TIME=pt_BR.UTF-8
-export LC_COLLATE="en_US.UTF-8"
-export LC_MONETARY=pt_BR.UTF-8
-export LC_MESSAGES="en_US.UTF-8"
-export LC_PAPER=pt_BR.UTF-8
-export LC_NAME=pt_BR.UTF-8
-export LC_ADDRESS=pt_BR.UTF-8
-export LC_TELEPHONE=pt_BR.UTF-8
-export LC_MEASUREMENT=pt_BR.UTF-8
-export LC_IDENTIFICATION=pt_BR.UTF-8
-export LC_ALL=
 EOF
 
 # Disable mnemonics in GTK apps to prevent weird issues with the Broadway
